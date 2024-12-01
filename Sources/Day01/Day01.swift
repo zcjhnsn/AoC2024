2027fa758f1c2bf33b8ffdd5a3544068cab91d1a
Gereon Steffens <github@steffens.org>
2024-12-01 08:26:38 +0100

    Day 1

--- Sources/Day01/Day01.swift
+++ Sources/Day01/Day01.swift
@@ -7,16 +7,35 @@
 import AoCTools
 
 final class Day01: AOCDay {
-    let title = "Day 1"
+    let title = "Historian Hysteria"
+    let list1: [Int]
+    let list2: [Int]
 
     init(input: String) {
+        var list1 = [Int]()
+        var list2 = [Int]()
+        for line in input.lines {
+            let ids = line.allInts()
+            list1.append(ids[0])
+            list2.append(ids[1])
+        }
+        self.list1 = list1
+        self.list2 = list2
     }
 
     func part1() -> Int {
-        0
+        zip(list1.sorted(by: <), list2.sorted(by: <))
+            .map { abs($0 - $1) }
+            .reduce(0, +)
     }
 
     func part2() -> Int {
-        0
+        var freq2 = [Int: Int]()
+        for i in list2 {
+            freq2[i, default: 0] += 1
+        }
+        return list1
+            .map { $0 * freq2[$0, default: 0] }
+            .reduce(0, +)
     }
 }
--- Tests/Day01Tests.swift
+++ Tests/Day01Tests.swift
@@ -6,6 +6,12 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 """
 
 @Suite("Day 1 Tests")
@@ -13,24 +19,24 @@ struct Day01Tests {
     @MainActor @Test("Day 1 Part 1")
     func testDay01_part1() {
         let day = Day01(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 11)
     }
 
     @MainActor @Test("Day 1 Part 1 Solution")
     func testDay01_part1_solution() {
         let day = Day01(input: Day01.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 1341714)
     }
 
     @MainActor @Test("Day 1 Part 2")
     func testDay01_part2() {
         let day = Day01(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 31)
     }
 
     @MainActor @Test("Day 1 Part 2 Solution")
     func testDay01_part2_solution() {
         let day = Day01(input: Day01.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 27384707)
     }
 }
