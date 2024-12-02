82748000b20da81b11fa69fe8989453cb85eac11
Gereon Steffens <github@steffens.org>
2024-12-02 10:13:54 +0100

    day 2

--- Sources/Day02/Day02.swift
+++ Sources/Day02/Day02.swift
@@ -5,17 +5,74 @@
 //
 
 import AoCTools
+import Algorithms
+
+private struct Report {
+    let data: [Int]
+    let pairs: [(Int, Int)]
+
+    init(data: [Int]) {
+        self.data = data
+        self.pairs = data.adjacentPairs().map { ($0.0, $0.1) }
+    }
+
+    var isSafe: Bool {
+        pairs.allSatisfy(increasing) || pairs.allSatisfy(decreasing)
+    }
+
+    var isSafeWithDampener: Bool {
+        if isSafe {
+            return true
+        }
+
+        let firstNonInc = pairs
+            .enumerated()
+            .first { index, element in !increasing(element.0, element.1) }
+        let firstNonDec = pairs
+            .enumerated()
+            .first { index, element in !decreasing(element.0, element.1) }
+
+        for check in [firstNonInc!, firstNonDec!] {
+            for index in [check.0, check.0 + 1] {
+                var data = self.data
+                data.remove(at: index)
+                let report = Report(data: data)
+                if report.isSafe {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    func increasing(_ a: Int, _ b: Int) -> Bool {
+        b > a && b - a < 4
+    }
+
+    func decreasing(_ a: Int, _ b: Int) -> Bool {
+        a > b && a - b < 4
+    }
+}
 
 final class Day02: AOCDay {
-    
+    let title = "Red-Nosed Reports"
+
+    private let reports: [Report]
+
     init(input: String) {
+        reports = input.lines.map { Report(data: $0.allInts()) }
     }
 
     func part1() -> Int {
-        0
+        reports
+            .filter { $0.isSafe }
+            .count
     }
 
     func part2() -> Int {
-        0
+        reports
+            .filter { $0.isSafeWithDampener }
+            .count
     }
 }
--- Tests/Day02Tests.swift
+++ Tests/Day02Tests.swift
@@ -6,6 +6,12 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 """
 
 @Suite("Day 2 Tests")
@@ -13,24 +19,24 @@ struct Day02Tests {
     @MainActor @Test("Day 2 Part 1")
     func testDay02_part1() {
         let day = Day02(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 2)
     }
 
     @MainActor @Test("Day 2 Part 1 Solution")
     func testDay02_part1_solution() {
         let day = Day02(input: Day02.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 411)
     }
 
     @MainActor @Test("Day 2 Part 2")
     func testDay02_part2() {
         let day = Day02(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 4)
     }
 
     @MainActor @Test("Day 2 Part 2 Solution")
     func testDay02_part2_solution() {
         let day = Day02(input: Day02.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 465)
     }
 }
