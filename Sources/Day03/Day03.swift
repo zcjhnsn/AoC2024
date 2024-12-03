893b8cf966d17fbaa1300fe92c42cf56c28dc43f
Gereon Steffens <github@steffens.org>
2024-12-03 08:10:53 +0100

    day 3

--- Sources/Day03/Day03.swift
+++ Sources/Day03/Day03.swift
@@ -5,17 +5,88 @@
 //
 
 import AoCTools
+import RegexBuilder
 
 final class Day03: AOCDay {
-    
+    let title = "Mull It Over"
+    let lines: [String]
+
     init(input: String) {
+        lines = input.lines
     }
 
     func part1() -> Int {
-        0
+        let x = Reference(Int.self)
+        let y = Reference(Int.self)
+        let regex = Regex {
+            "mul("
+            Capture(as: x) {
+                Repeat(1...3) {
+                    One(.digit)
+                }
+            } transform: { Int($0)! }
+            ","
+            Capture(as: y) {
+                Repeat(1...3) {
+                    One(.digit)
+                }
+            } transform: { Int($0)! }
+            ")"
+        }
+
+        return lines
+            .flatMap { line in
+                line.matches(of: regex).map {
+                    ($0[x], $0[y])
+                }
+            }
+            .map { $0.0 * $0.1 }
+            .reduce(0, +)
     }
 
     func part2() -> Int {
-        0
+        let x = Reference(Int.self)
+        let y = Reference(Int.self)
+        let regex = Regex {
+            ChoiceOf {
+                Regex {
+                    "mul("
+                    Capture(as: x) {
+                        Repeat(1...3) {
+                            One(.digit)
+                        }
+                    } transform: { Int($0)! }
+                    ","
+                    Capture(as: y) {
+                        Repeat(1...3) {
+                            One(.digit)
+                        }
+                    } transform: { Int($0)! }
+                    ")"
+                }
+                "do()"
+                "don't()"
+            }
+        }
+
+        var ignore = false
+        var pairs = [(Int, Int)]()
+        for line in lines {
+            let matches = line.matches(of: regex)
+            for match in matches {
+                switch match.output.0 {
+                case "don't()":
+                    ignore = true
+                case "do()":
+                    ignore = false
+                default:
+                    if !ignore {
+                        pairs.append((match[x], match[y]))
+                    }
+                }
+            }
+        }
+
+        return pairs.reduce(0) { $0 + $1.0 * $1.1 }
     }
 }
--- Tests/Day03Tests.swift
+++ Tests/Day03Tests.swift
@@ -6,6 +6,11 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+"""
+
+fileprivate let testInput2 = """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 """
 
 @Suite("Day 3 Tests")
@@ -13,24 +18,24 @@ struct Day03Tests {
     @MainActor @Test("Day 3 Part 1")
     func testDay03_part1() {
         let day = Day03(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 161)
     }
 
     @MainActor @Test("Day 3 Part 1 Solution")
     func testDay03_part1_solution() {
         let day = Day03(input: Day03.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 175015740)
     }
 
     @MainActor @Test("Day 3 Part 2")
     func testDay03_part2() {
-        let day = Day03(input: testInput)
-        #expect(day.part2() == 0)
+        let day = Day03(input: testInput2)
+        #expect(day.part2() == 48)
     }
 
     @MainActor @Test("Day 3 Part 2 Solution")
     func testDay03_part2_solution() {
         let day = Day03(input: Day03.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 112272912)
     }
 }
