74f8b602041472da27a159573545a42d82e40423
Zac Johnson <zacjohnson@protonmail.com>
2024-12-09 18:29:26 -0500

    day 7

--- Sources/Day07/Day07.swift
+++ Sources/Day07/Day07.swift
@@ -5,17 +5,84 @@
 //
 
 import AoCTools
+import Parsing
+
+private struct Instruction {
+    let test: Int
+    let numbers: [Int]
+
+    init(test: Int, numbers: [Int]) {
+        self.test = test
+        self.numbers = numbers
+    }
+
+    struct Parser: Parsing.Parser {
+        var body: some Parsing.Parser<Substring, Instruction> {
+            Parse(Instruction.init(test:numbers:)) {
+                Int.parser()
+                ": "
+                Many {
+                    Int.parser()
+                } separator: {
+                    " "
+                }
+            }
+        }
+    }
+}
 
 final class Day07: AOCDay {
-    
+    let title = "Bridge Repair"
+    private let instructions: [Instruction]
+
     init(input: String) {
+        let ins = Many {
+            Instruction.Parser()
+        } separator: {
+            "\n"
+        }
+
+        try! instructions = ins.parse(input)
+    }
+
+    private func solve(test: Int, operands: [Int], part2: Bool = false) -> Bool {
+        // does the remaining operand match test?
+        if operands.count == 1 { return operands.first == test }
+        // if the last operand is a factor of the test
+        // and the remaining factor is obtainable from the rest of the operands,
+        // the instruction is valid
+        if test % operands.last! == 0
+            && solve(test: test / operands.last!, operands: Array(operands.dropLast()), part2: part2) {
+            return true
+        }
+        // if the test is greater than the last operand
+        // and we can obtain the test - last operand difference from the rest of the operands,
+        // the instruction is valid
+        if test > operands.last! && solve(test: test - operands.last!, operands: Array(operands.dropLast()), part2: part2) { return true }
+
+        guard part2 else { return false }
+
+        let testString = String(test)
+        let lastString = String(operands.last!)
+        let nextTest = Int(String(testString.dropLast(lastString.count))) ?? 0
+
+        if testString.count > lastString.count
+            && testString.hasSuffix(lastString)
+            && solve(test: nextTest, operands: Array(operands.dropLast()), part2: part2) {
+            return true
+        }
+        return false
     }
 
     func part1() -> Int {
-        0
+        instructions
+            .filter { solve(test: $0.test, operands: $0.numbers) }
+            .sumOf(\.test)
     }
 
     func part2() -> Int {
-        0
+        instructions
+            .filter { solve(test: $0.test, operands: $0.numbers, part2: true) }
+            .sumOf(\.test)
     }
 }
--- Tests/Day07Tests.swift
+++ Tests/Day07Tests.swift
@@ -6,6 +6,15 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
 """
 
 @Suite("Day 7 Tests")
@@ -13,24 +22,24 @@ struct Day07Tests {
     @MainActor @Test("Day 7 Part 1")
     func testDay07_part1() {
         let day = Day07(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 3749)
     }
 
     @MainActor @Test("Day 7 Part 1 Solution")
     func testDay07_part1_solution() {
         let day = Day07(input: Day07.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 5702958180383)
     }
 
     @MainActor @Test("Day 7 Part 2")
     func testDay07_part2() {
         let day = Day07(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 11387)
     }
 
     @MainActor @Test("Day 7 Part 2 Solution")
     func testDay07_part2_solution() {
         let day = Day07(input: Day07.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 92612386119138)
     }
 }
