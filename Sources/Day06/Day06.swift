26171531f6687cbc819c20d7509b61e6a8063e7b
Zac Johnson <zacjohnson@protonmail.com>
2024-12-09 18:29:18 -0500

    day 6

--- Sources/Day06/Day06.swift
+++ Sources/Day06/Day06.swift
@@ -7,15 +7,73 @@
 import AoCTools
 
 final class Day06: AOCDay {
-    
+    let title = "Guard Gallivant"
+    let grid: Grid<Character>
+    let start: Point
+    let traversablePoints: Int
+
     init(input: String) {
+        self.grid = Grid<Character>.parse(input)
+        self.start = grid.points.first(where: { $1 == "^" })!.key
+        self.traversablePoints = grid.points.count { $0.value == "." }
     }
 
+    private func findPath(from start: Point) -> Set<Point> {
+        var visited: Set<Point> = [start]
+        var direction = Direction.n
+        var current = start
+        while true {
+            let next = current.moved(to: direction)
+            switch grid.points[next] {
+            case nil:
+                return visited
+            case "#":
+                direction = direction.turned(.clockwise)
+            case ".", "^":
+                visited.insert(next)
+                current = next
+            default: fatalError("Invalid character in path: \(grid.points[next]!)")
+            }
+        }
+    }
     func part1() -> Int {
-        0
+        findPath(from: start).count
     }
 
     func part2() -> Int {
-        0
+        var path = findPath(from: start)
+        path.remove(start)
+
+        // place an obstacle on every coordinate of the path
+        var loops = 0
+        for point in path {
+            var grid = self.grid.points
+            grid[point] = "#"
+
+            var current = start
+            var direction = Direction.n
+            var steps = 0
+            innerLoop: while true {
+                // taken more steps than traversable points
+                // must be in a loop
+                if steps > traversablePoints {
+                    loops += 1
+                    break
+                }
+                let next = current.moved(to: direction)
+                switch grid[next] {
+                case nil:
+                    break innerLoop
+                case "#":
+                    direction = direction.turned(.clockwise)
+                case ".", "^":
+                    current = next
+                    steps += 1
+                default: fatalError("Invalid character in path: \(grid[next]!)")
+                }
+            }
+        }
+
+        return loops
     }
 }
--- Tests/Day06Tests.swift
+++ Tests/Day06Tests.swift
@@ -6,6 +6,16 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
 """
 
 @Suite("Day 6 Tests")
@@ -13,24 +23,24 @@ struct Day06Tests {
     @MainActor @Test("Day 6 Part 1")
     func testDay06_part1() {
         let day = Day06(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 41)
     }
 
     @MainActor @Test("Day 6 Part 1 Solution")
     func testDay06_part1_solution() {
         let day = Day06(input: Day06.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 4580)
     }
 
     @MainActor @Test("Day 6 Part 2")
     func testDay06_part2() {
         let day = Day06(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 6)
     }
 
     @MainActor @Test("Day 6 Part 2 Solution")
     func testDay06_part2_solution() {
         let day = Day06(input: Day06.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 1480)
     }
 }
