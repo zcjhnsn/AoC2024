dce37b0f55c1152414eb78eb7f97df5139d412c0
Zac Johnson <zacjohnson@protonmail.com>
2024-12-10 10:18:54 -0500

    Day 10

--- Sources/Day10/Day10.swift
+++ Sources/Day10/Day10.swift
@@ -7,15 +7,63 @@
 import AoCTools
 
 final class Day10: AOCDay {
-    
+    let title = "Hoof it"
+
+    let grid: Dictionary<Point, Int>
+
     init(input: String) {
+        let lines = input.lines
+        let points = lines
+            .enumerated().flatMap { y, line in
+                line.enumerated().map { x, ch in
+                    let p = Point(x, y)
+                    return (p, Int(String(ch)) ?? -1)
+                }
+            }
+        grid = Dictionary(points, uniquingKeysWith: { _, new in new } )
     }
 
     func part1() -> Int {
-        0
+        let starts = grid.filter { $0.value == 0 }.keys.map { $0 }
+        let ends = grid.filter { $0.value == 9 }.keys.map { $0 }
+
+        let pathfinding = AStarPathfinder(map: Trail(grid: grid))
+
+        return starts.flatMap { start in
+            ends.map { end in
+                pathfinding.shortestPath(from: start, to: end).isEmpty ? 0 : 1
+            }
+        }
+        .sum()
     }
 
     func part2() -> Int {
-        0
+        let starts = grid.filter { $0.value == 0 }.keys.map { $0 }
+        let trail = Trail(grid: grid)
+
+        return starts.map { start in
+            trail.findRatings(from: start)
+        }
+        .sum()
+    }
+
+    struct Trail: Pathfinding {
+        let grid: [Point: Int]
+
+        func neighbors(for point: AoCTools.Point) -> [AoCTools.Point] {
+            let height = grid[point]!
+            return point
+                .neighbors()
+                .filter { grid[$0] == height + 1 }
+        }
+        
+        func findRatings(from start: Point, rating: Int = 0) -> Int {
+            if grid[start] == 9 {
+                return rating + 1
+            }
+
+            return neighbors(for: start)
+                .reduce(0) { $0 + findRatings(from: $1, rating: rating) }
+        }
     }
 }
--- Tests/Day10Tests.swift
+++ Tests/Day10Tests.swift
@@ -6,6 +6,14 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
 """
 
 @Suite("Day 10 Tests")
@@ -13,24 +21,24 @@ struct Day10Tests {
     @MainActor @Test("Day 10 Part 1")
     func testDay10_part1() {
         let day = Day10(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 36)
     }
 
     @MainActor @Test("Day 10 Part 1 Solution")
     func testDay10_part1_solution() {
         let day = Day10(input: Day10.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 512)
     }
 
     @MainActor @Test("Day 10 Part 2")
     func testDay10_part2() {
         let day = Day10(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 81)
     }
 
     @MainActor @Test("Day 10 Part 2 Solution")
     func testDay10_part2_solution() {
         let day = Day10(input: Day10.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 1045)
     }
 }
