f5de4c4a8c13a0ecd91aa90805de3a8ae747f2dc
Zac Johnson <zacjohnson@protonmail.com>
2024-12-12 15:15:36 -0500

    day 11

--- Sources/Day11/Day11.swift
+++ Sources/Day11/Day11.swift
@@ -5,17 +5,64 @@
 //
 
 import AoCTools
+import Foundation
 
 final class Day11: AOCDay {
+    let title = "Plutonian Pebbles"
+
+    var stones: [Int]
     
     init(input: String) {
+        stones = input.allInts()
+    }
+    
+    private struct Key: Hashable {
+        let stone: Int
+        let blinks: Int
+    }
+    
+    private var cache = [Key: Int]()
+
+    private func expand(stone: Int, blinks: Int) -> Int {
+        guard blinks > 0 else { return 1 }
+        
+        // return previously cached value if exists
+        let key = Key(stone: stone, blinks: blinks)
+        if let result = cache[key] {
+            return result
+        }
+        
+        let digits = String(stone).count
+        let result: Int
+        
+        if stone == 0 {
+            result = expand(stone: 1, blinks: blinks - 1)
+        } else if digits.isMultiple(of: 2) {
+            let power = pow(10, digits / 2)
+            result = expand(stone: stone / power, blinks: blinks - 1) + expand(stone: stone % power, blinks: blinks - 1)
+        } else {
+            result = expand(stone: stone * 2024, blinks: blinks - 1)
+        }
+        
+        cache[key] = result
+        return result
+    }
+
+    private func blink(times blinks: Int) -> Int {
+        stones
+            .map { expand(stone: $0, blinks: blinks) }
+            .sum()
     }
 
     func part1() -> Int {
-        0
+        blink(times: 25)
     }
 
     func part2() -> Int {
-        0
+        blink(times: 75)
+    }
+    
+    private func pow(_ x: Int, _ y: Int) -> Int {
+        Int(Foundation.pow(Double(x), Double(y)))
     }
 }
--- Tests/Day11Tests.swift
+++ Tests/Day11Tests.swift
@@ -6,6 +6,7 @@ import Testing
 @testable import AdventOfCode
 
 fileprivate let testInput = """
+125 17
 """
 
 @Suite("Day 11 Tests")
@@ -13,24 +14,24 @@ struct Day11Tests {
     @MainActor @Test("Day 11 Part 1")
     func testDay11_part1() {
         let day = Day11(input: testInput)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 55312)
     }
 
     @MainActor @Test("Day 11 Part 1 Solution")
     func testDay11_part1_solution() {
         let day = Day11(input: Day11.input)
-        #expect(day.part1() == 0)
+        #expect(day.part1() == 233050)
     }
 
     @MainActor @Test("Day 11 Part 2")
     func testDay11_part2() {
         let day = Day11(input: testInput)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 65601038650482)
     }
 
     @MainActor @Test("Day 11 Part 2 Solution")
     func testDay11_part2_solution() {
         let day = Day11(input: Day11.input)
-        #expect(day.part2() == 0)
+        #expect(day.part2() == 276661131175807)
     }
 }
